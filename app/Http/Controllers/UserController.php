18ee85b751427d6775b6a4e0b83363f8f7079356
fateh97 <46086174+fateh97@users.noreply.github.com>
2026-04-26 08:07:40 +0800

    add ceo for role required

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'role' => 'required|in:admin,staff'
+            'role' => 'required|in:admin,staff,ceo'
         ]);
 
         $user = User::create([
